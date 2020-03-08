819266a72a2e806dbb66dded086874e8bce68b71
Sergio Aragonés <sergioat7@gmail.com>
2020-03-08 20:30:44 +0100

    Created project

---
+++ GamerCollection/GamerCollection/Code/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  GamerCollection
+//
+//  Created by Sergio Aragonés on 08/03/2020.
+//  Copyright (c) 2020. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ GamerCollection/GamerCollection/Code/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GamerCollection
+//
+//  Created by Sergio Aragonés on 08/03/2020.
+//  Copyright (c) 2020. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
