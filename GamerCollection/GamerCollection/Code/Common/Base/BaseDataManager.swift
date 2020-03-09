2f3bcb69dfd44d0af0ed6e4789c21e538fea9143
Sergio Aragonés <sergioat7@gmail.com>
2020-03-09 19:03:33 +0100

    Created Loading screen

--- GamerCollection/GamerCollection/Code/Common/Base/BaseDataManager.swift
+++ GamerCollection/GamerCollection/Code/Common/Base/BaseDataManager.swift
@@ -3,7 +3,7 @@
 //  GamerCollection
 //
 //  Created by Sergio Aragonés on 08/03/2020.
-//  Copyright (c) 2020. All rights reserved.
+//  Copyright (c) 2020 Sergio Aragonés. All rights reserved.
 //
 
 import UIKit
--- GamerCollection/GamerCollection/Code/Common/Base/BaseViewController.swift
+++ GamerCollection/GamerCollection/Code/Common/Base/BaseViewController.swift
@@ -3,12 +3,21 @@
 //  GamerCollection
 //
 //  Created by Sergio Aragonés on 08/03/2020.
-//  Copyright (c) 2020. All rights reserved.
+//  Copyright (c) 2020 Sergio Aragonés. All rights reserved.
 //
 
 import UIKit
 
+protocol BaseViewProtocol: class {
+    
+    func showLoading()
+    func hideLoading()
+}
+
 class BaseViewController: UIViewController {
+    
+    var loadingScreen = LoadingScreen()
+    var isLoading: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +28,16 @@ class BaseViewController: UIViewController {
         
         let message = "Showing " + NSStringFromClass(self.classForCoder)
         print(message)
+    }
+    
+    func showLoading() {
+        loadingScreen.show(view: view)
+        isLoading = true
+    }
+    
+    func hideLoading() {
+        loadingScreen.hide(completion: nil)
+        isLoading = false
     }
     
 }
