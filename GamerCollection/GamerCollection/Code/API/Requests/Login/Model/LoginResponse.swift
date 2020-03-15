5d257db7041c1e928f46f5322b055dadc1e8b2c0
Sergio Aragonés <sergioat7@gmail.com>
2020-03-15 14:22:26 +0100

    Created Login request

--- GamerCollection/GamerCollection/Code/API/Requests/Login/Model/LoginResponse.swift
+++ GamerCollection/GamerCollection/Code/API/Requests/Login/Model/LoginResponse.swift
@@ -1,17 +1,11 @@
 //
-//  GamesApiClient.swift
+//  LoginResponse.swift
 //  GamerCollection
 //
 //  Created by Sergio Aragonés on 14/03/2020.
 //  Copyright (c) 2020 Sergio Aragonés. All rights reserved.
 //
 
-import UIKit
-
-protocol GamesApiClientProtocol {
-    
-}
-
-class GamesApiClient: GamesApiClientProtocol {
-    
+struct LoginResponse: Codable {
+    let token: String
 }
