af661c462c88368fe74955ff3059073fcb5f0146
Sergio Aragonés <sergioat7@gmail.com>
2020-03-20 20:31:13 +0100

    Created Format entity in CoreData

---
+++ GamerCollection/GamerCollection/Code/Common/Persistance/Entities/Format/Format+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Format+CoreDataClass.swift
+//  GamerCollection
+//
+//  Created by alumno on 20/03/2020.
+//  Copyright (c) 2020 Sergio Aragonés. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Format)
+public class Format: NSManagedObject {
+
+}
