018b0c7b314d680910484ecf46e767e00cd445f0
Sergio Aragonés <sergioat7@gmail.com>
2020-03-23 21:19:15 +0100

    Added fonts

---
+++ GamerCollection/GamerCollection/Code/Common/Extensions/UIFont/UIFont+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  UIFont+Extension.swift
+//  GamerCollection
+//
+//  Created by alumno on 23/03/2020.
+//
+
+import UIKit
+
+extension UIFont {
+    
+    // MARK: - Palatino-Bold
+    
+    class var bold14: UIFont {
+        return UIFont(name: "Palatino-Bold", size: 14.0)!
+    }
+    
+    class var bold18: UIFont {
+        return UIFont(name: "Palatino-Bold", size: 18.0)!
+    }
+    
+    // MARK: - Palatino-Roman
+    
+    class var roman14: UIFont {
+        return UIFont(name: "Palatino-Roman", size: 14.0)!
+    }
+    
+}
